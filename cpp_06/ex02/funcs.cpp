a02dbd0e8aa2a3a9726267677ddee5e92230751b
qaneee <vardanyanarev48@gmail.com>
2026-07-18 18:22:00 +0400

    cpp06

--- cpp_06/ex02/funcs.cpp
+++ cpp_06/ex02/funcs.cpp
@@ -18,11 +18,11 @@ Base * generate(void)
 void identify(Base* p)
 {
 	if (dynamic_cast<A*>(p))
-        std::cout << "A" << std::endl;
-    else if (dynamic_cast<B*>(p))
-        std::cout << "B" << std::endl;
-    else if (dynamic_cast<C*>(p))
-        std::cout << "C" << std::endl;
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
 	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p))
