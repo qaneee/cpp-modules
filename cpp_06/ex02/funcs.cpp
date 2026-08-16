7da7201af8e5d279e49fc4ca58a7fc22866d3d4e
Arévik <vardanyanarev48@gmail.com>
2026-08-16 13:04:20 +0400

    cpp06

--- cpp_06/ex02/funcs.cpp
+++ cpp_06/ex02/funcs.cpp
@@ -11,18 +11,13 @@ Base * generate(void)
 		return new A();
 	else if (rand == 1)
 		return new B();
-	else
-		return new C();
+	return new C();
 }
 
 void identify(Base* p)
 {
 	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(p))
-		std::cout << "C" << std::endl;
 	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p))
